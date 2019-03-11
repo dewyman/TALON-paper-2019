ee08e839c09b29378e4ea084c9e246a007ae6e83
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-11 16:23:05 -0700

    Updated to remove extra chromosome header text

--- refs/download_and_index_mouse_genome.sh
+++ refs/download_and_index_mouse_genome.sh
@@ -1,12 +1,17 @@
 # Download ENCODE human reference genome
+# This is time consuming- run in screen
+
 mkdir -p mm10
 cd mm10
 wget https://www.encodeproject.org/files/mm10_no_alt_analysis_set_ENCODE/@@download/mm10_no_alt_analysis_set_ENCODE.fasta.gz
 gunzip mm10_no_alt_analysis_set_ENCODE.fasta.gz
 
+# Remove extra information from fasta headers
+awk '{print $1}' mm10_no_alt_analysis_set_ENCODE.fasta > mm10.fa
+
 # Index
 module load samtools/1.3
-samtools faidx mm10_no_alt_analysis_set_ENCODE.fasta
+samtools faidx mm10.fa
 
 # Download index for STAR
 wget https://www.encodeproject.org/files/ENCFF483PAE/@@download/ENCFF483PAE.tar.gz
