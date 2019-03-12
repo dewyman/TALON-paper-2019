cb4a454641653721c80748494d25820c5a7495a5
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-11 21:34:19 -0700

    Script for downloading HepG2 short reads

---
+++ Illumina/HepG2/download_reads_from_ENCODE.sh
@@ -0,0 +1,5 @@
+wget https://www.encodeproject.org/files/ENCFF000DPU/@@download/ENCFF000DPU.fastq.gz
+mv ENCFF000DPU.fastq.gz HepG2_rep1_illumina_1.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF000DQB/@@download/ENCFF000DQB.fastq.gz
+mv ENCFF000DQB.fastq.gz HepG2_rep1_illumina_2.fastq.gz
