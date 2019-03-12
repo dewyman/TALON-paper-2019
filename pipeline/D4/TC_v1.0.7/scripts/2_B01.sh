2223dac943f2220ff09c51e625bdd00530ef03f3
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-11 17:40:39 -0700

    Added TranscriptClean cmds

---
+++ pipeline/D4/TC_v1.0.7/scripts/2_B01.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+#$ -q sam128
+#$ -N D4-TC-2_B01
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+
+set -e
+module load dwyman/anaconda/3
+source activate runTC
+cd /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01
+time python /data/users/dwyman/TranscriptClean-1.0.7/TranscriptClean.py --sam /pub/dwyman/TALON-paper-2019/pipeline/D4/Minimap2/2_B01/mapped_FLNC_noScaff.sam --genome /pub/dwyman/TALON-paper-2019/refs/hg38/hg38.fa --spliceJns /pub/dwyman/TALON-paper-2019/refs/TranscriptClean/gencode_v29_SJs.tsv --outprefix /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/2_B01 --variants /pub/dwyman/TALON-paper-2019/refs/TranscriptClean/00-common_all.vcf.gz --primaryOnly
+samtools view -H /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/2_B01_clean.sam > /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/canonical.sam
+samtools view ${OUTDIR}/${group}/${group}_clean.sam | awk '{if($(NF-1) !~ 0) print $0}' >> /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/canonical.sam
+samtools view -bS /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/canonical.sam | samtools sort > /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/sorted_canonical.bam
+samtools index /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/sorted_canonical.bam
+touch /pub/dwyman/TALON-paper-2019/pipeline/D4/TC_v1.0.7/2_B01/done
+source deactivate
---
+++ pipeline/D4/pipeline_commands.sh
@@ -0,0 +1,17 @@
+
+# Copy over all data
+WORKDIR=/pub/dwyman/TALON-paper-2019/pipeline/D4
+Minimap2=${WORKDIR}/Minimap2
+mkdir -p Minimap2
+cd $Minimap2
+
+mkdir -p $Minimap2/1_A01
+mkdir -p $Minimap2/2_B01
+cp /share/samdata/dwyman/PB69/Minimap2/1_A01/mapped_FLNC.sam $Minimap2/1_A01/.
+cp /share/samdata/dwyman/PB69/Minimap2/2_B01/mapped_FLNC.sam $Minimap2/2_B01/.
+
+# Run TC
+/share/samdata/dwyman/sequel_scripts/./make-TC-scripts.sh /pub/dwyman/TALON-paper-2019/pipeline/D4
+
+# Filter reads post-TC to keep reads that are canonical or annotated noncanonical
+
