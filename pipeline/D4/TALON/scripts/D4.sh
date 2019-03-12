9126a7ffff7b2f6050df144698808f3b68fa6ec9
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-12 00:07:21 -0700

    D4 TALON script

---
+++ pipeline/D4/TALON/scripts/D4.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#$ -q sam
+#$ -N D4-TALON
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+set -e
+module load dwyman/anaconda/3
+source activate mypython3.7.2
+cd /pub/dwyman/TALON-paper-2019/pipeline/D4/TALON
+cp /pub/dwyman/TALON-paper-2019/refs/TALON/unmodified_full_gencode_v29.db talon.db
+time python /pub/dwyman/TALON/talon.py --f config.csv                                              --db talon.db                                              --build hg38                                              --cov 0.9                                              --identity 0                                              --o indiv
+source deactivate
