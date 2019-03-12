5edf68185c8de93bc19f90141d4c5723b0b0cc66
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-11 21:55:34 -0700

    Download fasta seqs too

--- refs/download_gencodev29.sh
+++ refs/download_gencodev29.sh
@@ -1,1 +1,6 @@
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.annotation.gtf.gz
+# Comprehensive gene annotation (CHR)
+#wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.annotation.gtf.gz
+#gunzip gencode.v29.annotation.gtf
+
+# Download fasta sequences
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.transcripts.fa.gz
