28dc4f9d0b9881075f106639964f6c0cebd19f51
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-12 12:31:30 -0700

    Fixed script typo

--- pipeline/D5/TALON/scripts/D5.sh
+++ pipeline/D5/TALON/scripts/D5.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 #$ -q sam
-#$ -N D4-TALON
+#$ -N D5-TALON
 #$ -M dwyman@uci.edu
 #$ -m ea
 #$ -cwd
@@ -9,7 +9,7 @@
 set -e
 module load dwyman/anaconda/3
 source activate mypython3.7.2
-cd /pub/dwyman/TALON-paper-2019/pipeline/D4/TALON
+cd /pub/dwyman/TALON-paper-2019/pipeline/D5/TALON
 cp /pub/dwyman/TALON-paper-2019/refs/TALON/unmodified_full_gencode_v29_2019-03-12.db talon.db
 time python /pub/dwyman/TALON/talon.py --f config.csv                                              --db talon.db                                              --build hg38                                              --cov 0.9                                              --identity 0                                              --o indiv
 source deactivate
