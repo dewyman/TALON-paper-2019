fe27b565668734f0fe8889be642ce2553c02dc4b
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-11 17:47:26 -0700

    Added filtering command

--- pipeline/D4/pipeline_commands.sh
+++ pipeline/D4/pipeline_commands.sh
@@ -11,7 +11,9 @@ cp /share/samdata/dwyman/PB69/Minimap2/1_A01/mapped_FLNC.sam $Minimap2/1_A01/.
 cp /share/samdata/dwyman/PB69/Minimap2/2_B01/mapped_FLNC.sam $Minimap2/2_B01/.
 
 # Run TC
-/share/samdata/dwyman/sequel_scripts/./make-TC-scripts.sh /pub/dwyman/TALON-paper-2019/pipeline/D4
+/share/samdata/dwyman/sequel_scripts/./make-TC-scripts.sh $WORKDIR
 
 # Filter reads post-TC to keep reads that are canonical or annotated noncanonical
+/share/samdata/dwyman/sequel_scripts/./filter-TC-output.sh $WORKDIR
 
+# Run TALON
