d8c81a99f5940d921f4d4edf7ebfecd214e3c601
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-03-28 21:21:02 -0700

    Had problems with liftOver executable that I downloaded, so I replaced with a module load of kent instead

--- RNA-PET/liftover_RNA-PET.sh
+++ RNA-PET/liftover_RNA-PET.sh
@@ -1,9 +1,10 @@
 # Liftover the RNA-PET data from hg19 to hg38
+module load jje/kent
 set -e
 
 for f in data/*.bed; do
     name=$(basename $f .bed)
-    ./liftOver data/${name}.bed hg19ToHg38.over.chain data/${name}_hg38.bed data/${name}_unlifted.bed
+    liftOver data/${name}.bed hg19ToHg38.over.chain data/${name}_hg38.bed data/${name}_unlifted.bed
     rm data/${name}_unlifted.bed
 done
 
