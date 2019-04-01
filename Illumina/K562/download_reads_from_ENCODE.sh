fb5b9568b38ec59958f2326ae53b1632707a1a26
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-01 14:36:31 -0700

    Download K562 short reads

---
+++ Illumina/K562/download_reads_from_ENCODE.sh
@@ -0,0 +1,5 @@
+wget https://www.encodeproject.org/files/ENCFF001RWF/@@download/ENCFF001RWF.fastq.gz
+mv ENCFF001RWF.fastq.gz K562_rep1_illumina_1.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF001RWC/@@download/ENCFF001RWC.fastq.gz
+mv ENCFF001RWC.fastq.gz K562_rep1_illumina_2.fastq.gz
