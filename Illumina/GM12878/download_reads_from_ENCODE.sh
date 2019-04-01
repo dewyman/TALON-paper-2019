8f0ca1ab125df454e12703f53947e0c6d1112ed4
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-01 14:11:51 -0700

    Added script to download GM12878 short reads

---
+++ Illumina/GM12878/download_reads_from_ENCODE.sh
@@ -0,0 +1,5 @@
+wget https://www.encodeproject.org/files/ENCFF001RVT/@@download/ENCFF001RVT.fastq.gz
+mv ENCFF001RVT.fastq.gz GM12878_rep1_illumina_1.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF001RVZ/@@download/ENCFF001RVZ.fastq.gz
+mv ENCFF001RVZ.fastq.gz GM12878_rep1_illumina_2.fastq.gz
