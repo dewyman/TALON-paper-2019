493efdd5e653c1308c4a9ae2a6fa3abc4c077908
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-08 12:03:32 -0700

    Add Kallisto script for Rep2 of GM

---
+++ Illumina/GM12878/run_kallisto_rep2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#$ -q sam128
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+#$ -N kallisto_GM12878
+#$ -pe one-node-mpi 16
+#$ -R y
+
+
+module load kallisto/0.43.1
+cd /dfs2/pub/dwyman/TALON-paper-2019/Illumina/GM12878
+mkdir -p Kallisto/Rep2
+
+illumina1=GM12878_rep2_illumina_1.fastq.gz
+illumina2=GM12878_rep2_illumina_2.fastq.gz
+transcriptomeFasta=/dfs2/pub/dwyman/TALON-paper-2019/refs/gencode.v29.transcripts.fa.gz
+indexName=/dfs2/pub/dwyman/TALON-paper-2019/refs/Kallisto/gencode_v29.idx
+
+kallisto quant -i $indexName -t 16 -o Kallisto/Rep2 -b 100 $illumina1 $illumina2
