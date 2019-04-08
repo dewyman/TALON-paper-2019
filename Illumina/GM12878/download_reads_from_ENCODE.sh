42fa0f4e972fbb43c32a1036748f267662722291
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-08 11:39:32 -0700

    Added second rep

--- Illumina/GM12878/download_reads_from_ENCODE.sh
+++ Illumina/GM12878/download_reads_from_ENCODE.sh
@@ -1,5 +1,16 @@
-wget https://www.encodeproject.org/files/ENCFF001RVT/@@download/ENCFF001RVT.fastq.gz
-mv ENCFF001RVT.fastq.gz GM12878_rep1_illumina_1.fastq.gz
+# From https://www.encodeproject.org/experiments/ENCSR000AEH/
 
-wget https://www.encodeproject.org/files/ENCFF001RVZ/@@download/ENCFF001RVZ.fastq.gz
-mv ENCFF001RVZ.fastq.gz GM12878_rep1_illumina_2.fastq.gz
+## Replicate 1
+#wget https://www.encodeproject.org/files/ENCFF001RVT/@@download/ENCFF001RVT.fastq.gz
+#mv ENCFF001RVT.fastq.gz GM12878_rep1_illumina_1.fastq.gz
+
+#wget https://www.encodeproject.org/files/ENCFF001RVZ/@@download/ENCFF001RVZ.fastq.gz
+#mv ENCFF001RVZ.fastq.gz GM12878_rep1_illumina_2.fastq.gz
+
+## Replicate 2
+wget https://www.encodeproject.org/files/ENCFF001RVX/@@download/ENCFF001RVX.fastq.gz 
+mv ENCFF001RVX.fastq.gz GM12878_rep2_illumina_1.fastq.gz
+
+
+wget https://www.encodeproject.org/files/ENCFF001RWB/@@download/ENCFF001RWB.fastq.gz
+mv ENCFF001RWB.fastq.gz GM12878_rep2_illumina_2.fastq.gz
