f9c2b0923938a984923986599dd2df84dbb61754
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-19 16:58:13 -0700

    Fixed bug

--- PAS-seq/map_PAS-seq_reads.sh
+++ PAS-seq/map_PAS-seq_reads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -q sam128
-#$ -pe one-node-mpi 16
+#$ -pe one-node-mpi 32
 #$ -R y
 #$ -N STAR_PAS-seq
 #$ -M dwyman@uci.edu
@@ -9,12 +9,12 @@
 #$ -j y
 
 module load STAR/2.6.0c
-cd /dfs2/pub/dwyman/TALON-paper-2019/PAS-seq/data
-mkdir mapped_PAS
+cd /dfs3/pub/dwyman/TALON-paper-2019/PAS-seq/data
+mkdir -p mapped_PAS
 
 STAR --readFilesIn \
     data/PAS1_S1_L001_R1_001.fastq.gz,data/PAS1_S1_L002_R1_001.fastq.gz,data/PAS1_S1_L003_R1_001.fastq.gz,data/PAS1_S1_L004_R1_001.fastq.gz \
     --readFilesCommand zcat \
-    --outFileNamePrefix mapped/PAS_ \
-    --genomeDir /dfs2/pub/dwyman/TALON-paper-2019/refs/hg38/STAR_hg38_ENCODE \
-    --runThreadN 16
+    --outFileNamePrefix mapped_PAS/PAS_ \
+    --genomeDir /dfs3/pub/dwyman/TALON-paper-2019/refs/hg38/STAR_hg38_ENCODE \
+    --runThreadN 32
