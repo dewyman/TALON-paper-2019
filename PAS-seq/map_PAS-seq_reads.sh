286dabbe10219ec81121b52306daf0458ad584e6
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-22 15:05:39 -0700

    Fixed path bug

--- PAS-seq/map_PAS-seq_reads.sh
+++ PAS-seq/map_PAS-seq_reads.sh
@@ -13,7 +13,7 @@ cd /dfs3/pub/dwyman/TALON-paper-2019/PAS-seq/data
 mkdir -p mapped_PAS
 
 STAR --readFilesIn \
-    data/PAS1_S1_L001_R1_001.fastq.gz,data/PAS1_S1_L002_R1_001.fastq.gz,data/PAS1_S1_L003_R1_001.fastq.gz,data/PAS1_S1_L004_R1_001.fastq.gz \
+    PAS1_S1_L001_R1_001.fastq.gz,PAS1_S1_L002_R1_001.fastq.gz,PAS1_S1_L003_R1_001.fastq.gz,PAS1_S1_L004_R1_001.fastq.gz \
     --readFilesCommand zcat \
     --outFileNamePrefix mapped_PAS/PAS_ \
     --genomeDir /dfs3/pub/dwyman/TALON-paper-2019/refs/hg38/STAR_hg38_ENCODE \
