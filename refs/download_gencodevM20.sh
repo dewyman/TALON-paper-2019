c6cdc79eb137ded8ebd71844cdf628df84dfecb9
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-04-23 13:45:21 -0700

    Upgraded Mouse GENCODE version

--- refs/download_gencodevM20.sh
+++
@@ -1,6 +0,0 @@
-# Comprehensive gene annotation (CHR)
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M20/gencode.vM20.annotation.gtf.gz
-gunzip gencode.vM20.annotation.gtf.gz
-
-# transcript sequences
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M20/gencode.vM20.transcripts.fa.gz
---
+++ refs/download_gencodevM21.sh
@@ -0,0 +1,6 @@
+# Comprehensive gene annotation (CHR)
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M21/gencode.vM21.annotation.gtf.gz
+gunzip gencode.vM21.annotation.gtf.gz
+
+# transcript sequences
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M21/gencode.vM21.transcripts.fa.gz
