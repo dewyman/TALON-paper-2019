d8276ad817a5c65d43046f2847df6ef38a5941fe
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-05-06 11:04:36 -0700

    Added rep2

--- Illumina/K562/download_reads_from_ENCODE.sh
+++ Illumina/K562/download_reads_from_ENCODE.sh
@@ -3,3 +3,9 @@ mv ENCFF001RWF.fastq.gz K562_rep1_illumina_1.fastq.gz
 
 wget https://www.encodeproject.org/files/ENCFF001RWC/@@download/ENCFF001RWC.fastq.gz
 mv ENCFF001RWC.fastq.gz K562_rep1_illumina_2.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF001RWE/@@download/ENCFF001RWE.fastq.gz
+mv ENCFF001RWE.fastq.gz K562_rep2_illumina_1.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF001RWG/@@download/ENCFF001RWG.fastq.gz
+mv ENCFF001RWG.fastq.gz K562_rep2_illumina_2.fastq.gz
