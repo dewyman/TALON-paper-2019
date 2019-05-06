92898a637b83fa44cd293c60fa188294c5af726c
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-05-06 10:50:26 -0700

    Changed to Rep1 only

--- Illumina/HepG2/run_kallisto_rep1.sh
+++ Illumina/HepG2/run_kallisto_rep1.sh
@@ -4,18 +4,18 @@
 #$ -m ea
 #$ -cwd
 #$ -j y
-#$ -N kallisto_HepG2
+#$ -N kallisto_HepG2_R1
 #$ -pe one-node-mpi 8
 #$ -R y
 
 
 module load kallisto/0.43.1
 cd /pub/dwyman/TALON-paper-2019/Illumina/HepG2
-mkdir -p Kallisto
+mkdir -p Kallisto/Rep1
 
 illumina1=HepG2_rep1_illumina_1.fastq.gz
 illumina2=HepG2_rep1_illumina_2.fastq.gz
 transcriptomeFasta=/pub/dwyman/TALON-paper-2019/refs/gencode.v29.transcripts.fa.gz
 indexName=/pub/dwyman/TALON-paper-2019/refs/Kallisto/gencode_v29.idx
 
-kallisto quant -i $indexName -t 8 -o Kallisto -b 100 $illumina1 $illumina2
+kallisto quant -i $indexName -t 8 -o Kallisto/Rep1 -b 100 $illumina1 $illumina2
