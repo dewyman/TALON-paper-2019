abec58f0b747d350573070d16f63b3495a6cf120
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-05-06 10:48:21 -0700

    Added additional replicate to analysis

--- Illumina/HepG2/download_reads_from_ENCODE.sh
+++ Illumina/HepG2/download_reads_from_ENCODE.sh
@@ -3,3 +3,9 @@ mv ENCFF000DPU.fastq.gz HepG2_rep1_illumina_1.fastq.gz
 
 wget https://www.encodeproject.org/files/ENCFF000DQB/@@download/ENCFF000DQB.fastq.gz
 mv ENCFF000DQB.fastq.gz HepG2_rep1_illumina_2.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF000DPX/@@download/ENCFF000DPX.fastq.gz
+mv ENCFF000DPX.fastq.gz HepG2_rep2_illumina_1.fastq.gz
+
+wget https://www.encodeproject.org/files/ENCFF000DQE/@@download/ENCFF000DQE.fastq.gz
+mv ENCFF000DQE.fastq.gz HepG2_rep2_illumina_2.fastq.gz
---
+++ Illumina/HepG2/run_STAR_for_sjs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -q sam128
+#$ -pe one-node-mpi 16
+#$ -R y
+#$ -N STAR-HepG2
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+
+sample1_reads1=/pub/dwyman/TALON-paper-2019/Illumina/HepG2/HepG2_rep1_illumina_1.fastq.gz
+sample1_reads2=/pub/dwyman/TALON-paper-2019/Illumina/HepG2/HepG2_rep1_illumina_2.fastq.gz
+sample2_reads1=/pub/dwyman/TALON-paper-2019/Illumina/HepG2/HepG2_rep2_illumina_1.fastq.gz
+sample2_reads2=/pub/dwyman/TALON-paper-2019/Illumina/HepG2/HepG2_rep2_illumina_2.fastq.gz
+
+module load STAR/2.5.2a
+STAR --runThreadN 16 --genomeDir /pub/dwyman/TALON-paper-2019/refs/hg38/STAR_hg38_ENCODE \
+     --readFilesIn $sample1_reads1,$sample1_reads2,$sample2_reads1,$sample2_reads2 \
+     --sjdbGTFfile /pub/dwyman/TALON-paper-2019/refs/gencode.v29.annotation.gtf \
+     --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 \
+     --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 \
+     --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 \
+     --outFileNamePrefix /pub/dwyman/TALON-paper-2019/Illumina/HepG2/STAR \
+     --outSAMattributes NH HI NM MD jM jI --outSAMtype SAM --readFilesCommand zcat
