b22fae9e118e6ba514077641d6c5c44250cb6a6f
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-05-22 18:10:24 -0700

    local version

--- analysis_scripts/filter_kallisto_illumina_transcripts.R
+++ analysis_scripts/filter_kallisto_illumina_transcripts.R
@@ -9,8 +9,8 @@ filter_kallisto_illumina_transcripts <- function(kallisto_file) {
                                   col_names = TRUE, trim_ws = TRUE, na = "NA"))
 
     # Split GENCODE transcript multi-id by '|'
-    extraCols <- str_split_fixed(gencode.quantitation$target_id, "\\|",9)[,c(5,6,8)]
-    colnames(extraCols) <- c("transcript", "gene", "class")
+    extraCols <- str_split_fixed(gencode.quantitation$target_id, "\\|",9)[,c(5,6,8,1,2)]
+    colnames(extraCols) <- c("transcript", "gene", "class", "t_ID", "g_ID")
     gencode.quantitation <- cbind(extraCols, gencode.quantitation)
 
     # Remove transcripts that are < 300 bp in length because PacBio chucks anything that size, and
@@ -19,7 +19,7 @@ filter_kallisto_illumina_transcripts <- function(kallisto_file) {
     filtered_transcripts <- subset(gencode.quantitation, length >= 300 & tpm > 1)
 
 
-    mitochondrial_blacklist <- c("MT-TF", "MT-RNR1", "MT-TV", "MT-RNR2", "MT-TL1", "MT-ND1", "MT-TI", "MT-TQ", "MT-TM", "MT-ND2", "MT-TW", "MT-TA", "MT-TN", "MT-TC", "MT-TY", "MT-CO1", "MT-TS1", "MT-TD", "MT-CO2", "MT-TK", "MT-ATP8", "MT-ATP6", "MT-CO3", "MT-TG", "MT-ND3", "MT-TR", "MT-ND4L", "MT-ND4", "MT-TH", "MT-TS2", "MT-TL2", "MT-ND5", "MT-ND6", "MT-CYB", "MTATP6P1", "MT-CO3")
+    mitochondrial_blacklist <- c("MT-TF", "MT-RNR1", "MT-TV", "MT-RNR2", "MT-TL1", "MT-ND1", "MT-TI", "MT-TQ", "MT-TM", "MT-ND2", "MT-TW", "MT-TA", "MT-TN", "MT-TC", "MT-TY", "MT-CO1", "MT-TS1", "MT-TD", "MT-CO2", "MT-TK", "MT-ATP8", "MT-ATP6", "MT-CO3", "MT-TG", "MT-ND3", "MT-TR", "MT-ND4L", "MT-ND4", "MT-TH", "MT-TS2", "MT-TL2", "MT-ND5", "MT-ND6", "MT-CYB", "MTATP6P1")
     final_filtered_transcripts <- subset(filtered_transcripts, !(gene %in% mitochondrial_blacklist))
 
     # Normalize back to 1 million transcripts
