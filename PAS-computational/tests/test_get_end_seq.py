00e4a6eca5606dc541717ce14481f4076a24c8e3
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-05-24 14:34:26 -0700

    Added test for + strand

--- PAS-computational/tests/test_get_end_seq.py
+++ PAS-computational/tests/test_get_end_seq.py
@@ -11,6 +11,26 @@ import create_intervals as cI
 import get_last_n_transcript_seq as glt 
 
 class TestGetEndSeq(object):
+    def test_plus_strand(self):
+        """ Get sequence at the end of plus strand transcript"""
+
+        entry = ['chr1', '827666', '841738', 'ENST00000441765.5', '0', '+']
+        chromosome = entry[0]
+        strand = entry[-1]
+        dist = 10
+
+        genome_file = "chr1.fa"
+        genome = pf.Fasta(genome_file)
+
+        # Get interval for end
+        interval_start, interval_end = glt.make_end_interval(entry, dist)
+        assert interval_start == 841728
+        assert interval_end == 841738
+
+        # Get sequence for end
+        seq = glt.fetch_sequence(chromosome, interval_start, interval_end, strand, genome)
+        assert seq == "ATGAATATCT"
+
     def test_minus_strand(self):
         """ Get sequence at the end of minus strand transcript"""
 
