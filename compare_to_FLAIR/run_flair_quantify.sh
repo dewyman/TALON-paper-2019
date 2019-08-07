11bf96749a3e2ae19b6d72560c6c6810c8ec429e
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-08-07 13:10:27 -0700

    config.tsv

---
+++ compare_to_FLAIR/run_flair_quantify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$ -q sam
+#$ -pe one-node-mpi 4
+#$ -R y
+#$ -N GM12878_flair_quantify
+#$ -M dwyman@uci.edu
+#$ -m ea
+#$ -cwd
+#$ -j y
+
+module load samtools
+module load bedtools
+mkdir -p tmp
+
+cd /pub/dwyman/TALON-paper-2019/compare_to_FLAIR
+
+config=config.tsv
+isoforms=flair.collapse.isoforms.fa
+
+python ~/flair/flair.py quantify -i $isoforms \
+                              -t 4 \
+                              -m ~/minimap2-2.15 \
+                              -r $config \
+                              --tpm \
+                              --temp_dir tmp
