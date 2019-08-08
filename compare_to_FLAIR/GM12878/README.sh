c3a86d378949b0abc28f42e2d3ce5247d4e92648
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-08-08 10:56:32 -0700

    Edited README

--- compare_to_FLAIR/GM12878/README.sh
+++ compare_to_FLAIR/GM12878/README.sh
@@ -1,11 +1,33 @@
-
-
-
-In order to determine how well PacBio + FLAIR detects genes known to be expressed in short-read data, we converted the FLAIR output to a TALON-like format, and then ran a custom R script:
+# Running FLAIR on GM12878 PacBio data
+1. Run align and correct steps separately on replicates
 ```
-python format_flair_matrix_like_talon.py counts_matrix.tsv counts_matrix_talon_abd.tsv
+qsub D8/./run_FLAIR_align.sh
+qsub D9/./run_FLAIR_align.sh
+```
+```
+qsub D8/./run_FLAIR_correct.sh
+qsub D9/./run_FLAIR_correct.sh
+```
+2. Then, run collapse step on concatenated files from both reps.
+```
+cat D8/flair_all_corrected.psl D9/flair_all_corrected.psl > D8-D9_flair_all_corrected.psl
+cat D8/ENCFF281TNJ.fastq D9/D9/ENCFF475ORL.fastq > D8-D9-concat.fastq
+qsub ./run_flair_collapse.sh
+```
+3. Finally, run quantify step. To do this, you need to create a tab-delimited config file with fields dataset name, condition, batch, and fastq reads file. This is what the GM12878 file looks like:
+```
+GM12878_Rep1	GM12878	batch1	/pub/dwyman/TALON-paper-2019/compare_to_FLAIR/D8/ENCFF281TNJ.fastq
+GM12878_Rep2	GM12878	batch1	/pub/dwyman/TALON-paper-2019/compare_to_FLAIR/D9/ENCFF475ORL.fastq
+```
+```
+qsub ./run_flair_quantify.sh
+```
 
-Rscript plot_detection_by_TPM_for_datasets.R \
+4. In order to determine how well PacBio + FLAIR detects genes known to be expressed in short-read data, we converted the FLAIR output to a TALON-like format, and then ran a custom R script:
+```
+python ../format_flair_matrix_like_talon.py counts_matrix.tsv counts_matrix_talon_abd.tsv
+
+Rscript ../plot_detection_by_TPM_for_datasets.R \
       --f counts_matrix_talon_abd.tsv \
       --datasets GM12878_Rep1_GM12878_batch1,GM12878_Rep2_GM12878_batch1 \
       --ik1 ../Illumina/GM12878/Kallisto/Rep1/abundance.tsv \
@@ -13,3 +35,4 @@ Rscript plot_detection_by_TPM_for_datasets.R \
       --color blue \
       -o .
 ```
+<img align="left" width="200" src="FLAIR/gene_detection_by_TPM.png">
