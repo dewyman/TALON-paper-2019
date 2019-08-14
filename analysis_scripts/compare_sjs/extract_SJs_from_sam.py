5e9a2287bd7d1a03f8e4f310346802b908e347e3
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-08-14 13:08:43 -0700

    Completed

--- analysis_scripts/compare_sjs/extract_SJs_from_sam.py
+++ analysis_scripts/compare_sjs/extract_SJs_from_sam.py
@@ -76,6 +76,76 @@ def get_introns(start, cigar):
 
     return intron_coords
 
+def get_splice_seq(chrom, pos, bound, genome):
+        """ The splice motif consists of the first two or last two bases of the
+            intron (first two if bound == 0 and last two if bound == 1).
+            Bound = 0 refers to the left side of the jn, and bound = 1 refers to
+            the right side (forward orientation) """
+
+        if bound == 0:
+            motif = genome.sequence({'chr': chrom, 'start': pos, 
+                                     'stop': pos + 1}, one_based=True)
+        else:
+            motif = genome.sequence({'chr': chrom, 'start': pos - 1, 
+                                     'stop': pos}, one_based=True)
+        return motif
+
+def getSJMotifCode(startBases, endBases):
+    """ Determines which STAR-style splice junction code applies to a splice motif """       
+
+    motif = (startBases + endBases).upper()
+
+    if motif == "GTAG":
+        return 1
+    elif motif == "CTAC":
+        return 2
+    elif motif == "GCAG":
+        return 3
+    elif motif == "CTGC":
+        return 4
+    elif motif == "ATAC":
+        return 5
+    elif motif == "GTAT":
+        return 6
+    else:
+        return 0
+
+def fetch_splice_motif_code(chrom, start_pos, end_pos, strand, genome):
+    """ Use Pyfasta to extract the splice motif sequence based on the start
+        and end of the splice junction. Then, convert this sequence motif 
+        to a numeric code. """
+
+    start_motif = get_splice_seq(chrom, start_pos, 0, genome)
+    end_motif = get_splice_seq(chrom, end_pos, 1, genome)
+
+    motif_code = getSJMotifCode(start_motif, end_motif)
+    return motif_code
+
+def create_sj_tuples(chrom, strand, intron_coords, genome):
+    """ Walk through intron coord pairs to assemble SJ tuples for each:
+            (chr, start, end, strand, splice_motif)
+        Since we want the smallest coordinate first in each case, we do not 
+        need to orient based on strand."""
+
+    sj_tuples = []
+
+    start_index = 0
+    while start_index < len(intron_coords) -1 :
+        end_index = start_index + 1
+
+        start_pos = intron_coords[start_index]
+        end_pos = intron_coords[end_index]
+
+        motif = fetch_splice_motif_code(chrom, start_pos, end_pos, strand, genome)
+
+        # Assemble the tuple
+        sj = (chrom, start_pos, end_pos, strand, motif)
+        sj_tuples.append(sj)
+
+        start_index += 2
+
+    return sj_tuples
+
 def get_SJs_from_read(read_fields, genome):
     """ Working from the SAM fields, first compute intron positions and splice 
         motifs (1-based). Then, iterate over pairs of positions to create the 
@@ -85,25 +155,22 @@ def get_SJs_from_read(read_fields, genome):
                  One tuple per junction.
     """
 
-    sjs = []
-    
     # Compute each splice junction position and splice motif
     FLAG = int(read_fields[1])
     chrom = read_fields[2]
     start = int(read_fields[3])
+    CIGAR = read_fields[5]
+    intron_coords = get_introns(start, CIGAR)
+
     if FLAG == 0:
         strand = 1
     elif FLAG == 16:
         strand = 2
     else:
-        strand = 0
-
-    CIGAR = read_fields[5]
-    intron_coords = get_introns(start, CIGAR)
-    print(intron_coords)
-
+        strand = 0 
     
-    
+    sjs = create_sj_tuples(chrom, strand, intron_coords, genome)
+    return sjs
 
 def main():
     options = getOptions()
@@ -115,8 +182,10 @@ def main():
     genome = pf.Fasta(fasta)
     
     # Initialize data structures
+    sj_count = {}
 
     # Iterate over each transcript. Disregard if it isn't a primary mapper
+    print("Parsing junctions...")
     with open(sam_file, 'r') as f:
         for read in f:
             read = read.strip() 
@@ -130,11 +199,21 @@ def main():
             # Compute each splice junction position and splice motif
             list_of_sjs = get_SJs_from_read(read_fields, genome)
 
-            # Add to dict to track number of reads containing the junction
+            # Add each junction to dict in order to track number of reads 
+            # that have it
+            for sj in list_of_sjs:
+                try:
+                    sj_count[sj] += 1
+                except:
+                    sj_count[sj] = 1 
 
-
-    # Output all of the junctions to a file     
-
+    # Output all of the junctions to a file
+    print("Writing junctions to output...")
+    o = open(outprefix + "_SJs.txt", 'w')     
+    for junction, count in sj_count.items():
+        cols = [str(x) for x in junction] + ["0", str(count), "NA", "NA"]
+        o.write("\t".join(cols) + "\n")
+    o.close()    
 
 
 if __name__ == '__main__':
