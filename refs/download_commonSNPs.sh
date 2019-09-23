a6a367c6f1852443f488301f925793cb4925f1e1
Dana Elizabeth Wyman <dwyman@compute-2-17.local>
2019-09-23 16:35:06 -0700

    Added awk command that changes the chromosome naming convention

--- refs/download_commonSNPs.sh
+++ refs/download_commonSNPs.sh
@@ -3,3 +3,10 @@
 mkdir -p TranscriptClean
 cd TranscriptClean
 wget ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-common_all.vcf.gz
+
+# Change chromosome convention so that chromosomes start with 'chr'
+zcat 00-common_all.vcf.gz | \
+    awk '{if($0 !~ /^#/ && $0 !~ /^chr/) print "chr"$0; else print $0}' \
+    > tmp_00-common_all.vcf
+gzip tmp_00-common_all.vcf
+mv tmp_00-common_all.vcf.gz 00-common_all.vcf.gz
