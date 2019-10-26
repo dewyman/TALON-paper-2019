5f602eababc97af34e0c914a8ce43a6753e68ddf
fairliereese <fairliek@comcast.net>
2019-10-26 15:49:51 -0700

    added hippocampus/cortex illumina mapping scripts

--- splicing_analyses/run_STAR_illumina_cortex_Rep1.sh
+++ splicing_analyses/run_STAR_illumina_cortex_Rep1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -q som,bio,free64
+#$ -q som
 #$ -pe one-node-mpi 16
 #$ -R y
 #$ -m ea
@@ -8,18 +8,17 @@
 #$ -o /data/users/freese/mortazavi_lab/qsub_output
 #$ -e /data/users/freese/mortazavi_lab/qsub_output
 #$ -ckpt restart
-#$ -N hippo_st
+#$ -N map_cortex_1
 
 DPATH=~/mortazavi_lab/bin/TALON-paper-2019/analysis_scripts/compare_sjs/mouse_brain/
-curpath=~/mortazavi_lab/bin/TALON-paper-2019/splicing_analyses/technology_SJ_comparison/
-reads="${DPATH}Hp1A_R1.fastq.gz,${DPATH}Hp2A_R1.fastq.gz ${DPATH}Hp1A_R2.fastq.gz,${DPATH}Hp2A_R2.fastq.gz"
+reads="${DPATH}Cx1a_IlPB82_R1.fastq.gz ${DPATH}Cx1a_IlPB82_R2.fastq.gz"
 
 module load STAR/2.5.2a
 STAR \
 	--runThreadN 8 \
-	--genomeDir ~/mortazavi_lab/ref/hg38/STAR_hg38_ENCODE/ \
+	--genomeDir ~/mortazavi_lab/ref/mm10/STAR_mm10_ENCODE/ \
 	--readFilesIn $reads \
-	--sjdbGTFfile ~/mortazavi_lab/ref/gencode.v29/gencode.v29.annotation.gtf \
+	--sjdbGTFfile ~/mortazavi_lab/ref/gencode.vM21/gencode.vM21.annotation.gtf \
 	--outFilterType BySJout \
 	--outFilterMultimapNmax 20 \
 	--alignSJoverhangMin 8 \
@@ -29,7 +28,7 @@ STAR \
 	--alignIntronMin 20 \
 	--alignIntronMax 1000000 \
 	--alignMatesGapMax 1000000 \
-	--outFileNamePrefix ${curpath}hippocampus_aligned \
+	--outFileNamePrefix cortex_Rep1_aligned \
 	--outSAMattributes NH HI NM MD jM jI \
 	--outSAMtype SAM \
 	--readFilesCommand zcat
